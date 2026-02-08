aaa8300d998c931468c1107f61c88e935993eda6
Monika-Dangar <dangarmonika191023@gmail.com>
2026-02-08 13:42:12 +0530

    Frontend url added for cors

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: 'https://cogni-clear-frontend.vercel.app' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
